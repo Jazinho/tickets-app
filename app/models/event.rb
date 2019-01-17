0c913fdae2956efa5bee7c8a9198658717f7a056
jan <jan.palucki@gmail.com>
2019-01-17 21:33:23 +0100

    added one-to-many relation between event and tickets

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,5 @@
 class Event < ApplicationRecord
   validates :artist, :price_low, :price_high, :event_date, presence: true
   validates :price_low, :price_high, numericality: true
+  has_many :tickets
 end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,5 @@
 class Ticket < ApplicationRecord
   validates :name, :email_address, :price, presence: true
   validates :name, length: { :minimum => 6 }
+  belongs_to :event
 end
