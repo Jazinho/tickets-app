06b68d03c576f9e422794119f0cabb3c3b9c8525
jan <jan.palucki@gmail.com>
2019-01-20 10:32:49 +0100

    events validation

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,15 @@
 class Event < ApplicationRecord
+  def event_date_not_from_past
+        if event_date < Date.today
+            errors.add('Data wydarzenia', 'nie może być z przeszłości')
+        end
+  end
+
   validates :artist, :price_low, :price_high, :event_date, presence: true
   validates :price_low, :price_high, numericality: true
+
+  validates_numericality_of :price_high, :greater_than => :price_low
+  validate :event_date_not_from_past
+
   has_many :tickets
 end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,5 +1,13 @@
 class Ticket < ApplicationRecord
   validates :name, :email_address, :price, presence: true
   validates :name, length: { :minimum => 6 }
+
+  def price_in_scope
+        if (price > event.price_high) || (price < event.price_low)
+            errors.add('Cena', 'Cena biletu nie miesci sie w podanym zakresie cenowym')
+        end
+  end
+  validate :price_in_scope
+
   belongs_to :event
 end
